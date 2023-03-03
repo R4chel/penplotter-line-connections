b3b0f9230231b0753d66c532f8adedf273cd2313
R4chel <rae84@cornell.edu>
2023-03-03 13:51:14 -0500

    implement line

--- sketch_connectors.py
+++ sketch_connectors.py
@@ -1,5 +1,12 @@
 import vsketch
-from shapely.geometry import Point
+from shapely.geometry import Point, LineString
+
+
+class Connection:
+    def line(vsk,p1,p2):
+        return LineString([ p1,p2 ])
+
+connection_kind = {"line":Connection.line}   
 
 class ConnectorsSketch(vsketch.SketchClass):
     # Sketch parameters:
@@ -10,6 +17,8 @@ class ConnectorsSketch(vsketch.SketchClass):
     landscape = vsketch.Param(True)
     pen_width = vsketch.Param(0.7, decimals=3, min_value=1e-10, unit="mm")
     num_layers = vsketch.Param(1)
+    path_kind = vsketch.Param(next(iter(connection_kind.keys())), choices=connection_kind.keys())
+    
     # radius = vsketch.Param(1.0, decimals=3, unit="in")
 
     def random_point(self, vsk: vsketch.Vsketch):
@@ -23,11 +32,13 @@ class ConnectorsSketch(vsketch.SketchClass):
         vsk.penWidth(f"{self.pen_width}")
 
         # implement your sketch here
-        # layers = [1 + i for i in range(self.num_layers)]
-        # layer = layers[int(vsk.random(0, len(layers)))]
-        # vsk.stroke(layer)
-        # vsk.fill(layer)
-        # vsk.circle(0, 0, self.radius, mode="radius")
+        layers = [1 + i for i in range(self.num_layers)]
+        layer = layers[int(vsk.random(0, len(layers)))]
+        vsk.stroke(layer)
+        p1 = self.random_point(vsk)
+        p2 = self.random_point(vsk)
+        shape = connection_kind[self.path_kind](vsk,p1,p2)
+        vsk.geometry(shape)
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
