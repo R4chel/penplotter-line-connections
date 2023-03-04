0e2e8d74b21fa7e3903090c534b312e9ea18a171
R4chel <rae84@cornell.edu>
2023-03-04 16:48:52 -0500

    mid refactoring

--- sketch_connectors.py
+++ sketch_connectors.py
@@ -1,33 +1,52 @@
 import vsketch
 from shapely.geometry import GeometryCollection, Point, LineString, MultiLineString
 from shapely import get_parts, get_coordinates
+import numpy as np
+
 
 class Connection:
 
-    def line(vsk, p1, p2):
+    def line(_vsk, p1, p2):
         return LineString([p1, p2])
 
     def points(vsk, p1, p2):
-        max_distance= 10;
+        max_distance = 10
         path = Connection.line(vsk, p1, p2).segmentize(max_distance)
         points = [Point(p) for p in get_coordinates(path)]
+
         return points
+
+    def pts(vsk, p1, p2, max_length):
+        distance = p1.distance(p2)
+        num_steps = int(np.ceil(distance / max_length))
+        return np.linspace(np.array([p1.x, p1.y]),
+                           np.array([p2.x, p2.y]),
+                           num=num_steps,
+                           endpoint=True)
 
     def circles(vsk, p1, p2):
         points = Connection.points(vsk, p1, p2)
         radius = points[0].distance(points[1]) / 2
-        return GeometryCollection([p.buffer(radius) for p in points] )
+        return GeometryCollection([p.buffer(radius) for p in points])
 
     def dashes(vsk, p1, p2):
-        points = Connection.points(vsk, p1,p2)
+        points = Connection.points(vsk, p1, p2)
         # This is a bit wrong if for one of odd or even length linestrings but I'm not fixing it now
-        return MultiLineString([[points[i], points[i+1]] for i in range(0, len(points)-1, 2) ])
+        return MultiLineString([[points[i], points[i + 1]]
+                                for i in range(0,
+                                               len(points) - 1, 2)])
+
+    def tbd(vsk, p1, p2):
+        pts = Connection.pts(vsk, p1, p2, 10)
+        r = 5
+        return GeometryCollection([Point(p).buffer(r) for p in pts])
 
 
 connection_kind = {
     "line": Connection.line,
     "circles": Connection.circles,
     "dashes": Connection.dashes,
+    "tbd": Connection.tbd
 }
 
 
